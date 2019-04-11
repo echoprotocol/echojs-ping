7f803d43fd16a5c031e46a90dbb4ae21d9a874cb
Viktor Pulyak <viktor.pulyak@pixelplex.io>
2019-04-11 08:05:59 +0300

    Change lisening to 0.0.0.0

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const http = require('http');
 const { getRandomInt } = require('./src/helper');
 const IconColor = require('./src/icon.color');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
